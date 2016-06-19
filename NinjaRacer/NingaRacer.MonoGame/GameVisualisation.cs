c363d1bacde392e81a12d0c9275e0c60bc742209
dushka-dragoeva <d.dragoeva@abv.bg>
2016-06-19 08:40:43 +0300

    Added initial game architecture

---
+++ NinjaRacer/NingaRacer.MonoGame/GameVisualisation.cs
@@ -0,0 +1,12 @@
+﻿namespace NingaRacer.MonoGame
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class GameVisualisation
+    {
+    }
+}
---
+++ NinjaRacer/NinjaRacer.Models/Vechicles/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace NinjaRacer.Models.Vechicles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Car
+    {
+    }
+}
