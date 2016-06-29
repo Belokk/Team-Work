11bc251ad79c32d0ce24251954bac2e3af02f748
dushka-dragoeva <d.dragoeva@abv.bg>
2016-06-29 20:30:25 +0300

    added test class

---
+++ NinjaRacer/NinjaRacer/CheckGit.cs
@@ -0,0 +1,15 @@
+﻿namespace NinjaRacer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class CheckGit
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
